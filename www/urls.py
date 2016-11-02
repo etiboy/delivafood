1996562267f26bb4cdf5c6240c527355080fd45f
Etiboy <etiboydemas@gmail.com>
2016-11-02 10:56:48 +0530

    updated  food details

--- www/urls.py
+++ www/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
-
+app_name = 'www'
 urlpatterns = [
-	url(r'^$', views.home, name='home')
+	url(r'^$', views.home, name='home'),
+	#url(r'^food/(?P<food_name>.*)/(?P<food_id>.*)/$', views.foodDetails, name='food_details')
+	url(r'^food/(?P<food_id>.*)/(?P<food_name>.*)/$', views.foodDetails, name='food_details')
 ]
--- www/views.py
+++ www/views.py
@@ -6,3 +6,7 @@ from .models import Kitchen,Food,Area,Cuisine,FoodCategory
 def home(request):
 	homePageMeals = Food.objects.filter(kitchen__area__code__contains = '560')
 	return render(request, 'index.html', {'meals': homePageMeals})
+
+def foodDetails(request,food_id,food_name):
+	foodDetail = Food.objects.get(id  = food_id)
+	return render(request, 'food.html', {'food': foodDetail})
