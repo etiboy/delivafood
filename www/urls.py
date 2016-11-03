45c41292dd7bc77f8f55fe6ba96285f2f950e33e
Etiboy <etiboydemas@gmail.com>
2016-11-03 10:03:07 +0530

    Updated urls and views

--- www/urls.py
+++ www/urls.py
@@ -3,5 +3,6 @@ from . import views
 app_name = 'www'
 urlpatterns = [
 	url(r'^$', views.home, name='home'),
+	url(r'^area/(?P<area_code>.*)/(?P<area_name>.*)/$', views.home, name='home'),
 	url(r'^food/(?P<food_id>.*)/(?P<food_name>.*)/$', views.foodDetails, name='food_details')
 ]
--- www/views.py
+++ www/views.py
@@ -3,8 +3,8 @@ from .models import Kitchen,Food,Area,Cuisine,FoodCategory
 
 # home page
 
-def home(request):
-	homePageMeals = Food.objects.filter(kitchen__area__code__contains = '560')
+def home(request,area_name,area_code='560001'):
+	homePageMeals = Food.objects.filter(kitchen__area__code = area_code)
 	return render(request, 'index.html', {'meals': homePageMeals})
 
 def foodDetails(request,food_id,food_name):
