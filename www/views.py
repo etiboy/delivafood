6fe40c82c1ed472db69b88ff75a22d5d9984360c
Etiboy <etiboydemas@gmail.com>
2016-11-04 11:06:45 +0530

    updated model and tried some slugification

--- www/views.py
+++ www/views.py
@@ -1,12 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from django.http import Http404
 from .models import Kitchen,Food,Area,Cuisine,FoodCategory
 
 # home page
 
-def home(request,area_name,area_code='560001'):
-	homePageMeals = Food.objects.filter(kitchen__area__code = area_code)
+def home(request):
+	homePageMeals = Food.objects.filter(kitchen__area__code__contains = "560")
 	return render(request, 'index.html', {'meals': homePageMeals})
 
 def foodDetails(request,food_id,food_name):
-	foodDetail = Food.objects.get(id  = food_id)
+	foodDetail = get_object_or_404(Food, pk=food_id)
+	'''
+	try:
+		foodDetail = Food.objects.get(id  = food_id)
+	except Food.DoesNotExist:
+		 raise Http404("Question does not exist")
+	'''
 	return render(request, 'food.html', {'food': foodDetail})
